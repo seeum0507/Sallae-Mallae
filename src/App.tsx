44c52e1f92936355bb24f1771731ffe7c8f3f1ad
seeum0507 <kwonstella0507@gmail.com>
2026-05-27 08:48:03 +0900

    authProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 import { TopNav } from "./components/TopNav";
 import { HomePage } from "./pages/HomePage";
 import { SearchResultsPage } from "./pages/SearchResultsPage";
@@ -7,25 +8,28 @@ import { ProductDetailPage } from "./pages/ProductDetailPage";
 import { LoginPage } from "./pages/LoginPage";
 import { SignupPage } from "./pages/SignupPage";
 import { MyPage } from "./pages/MyPage";
-import { PhotoReviewsPage } from "./pages/PhotoReviewsPage";
 import { AllReviewsPage } from "./pages/AllReviewsPage";
+import { PhotoReviewsPage } from "./pages/PhotoReviewsPage";
+
 export function App() {
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-white selection:bg-mint-200 selection:text-mint-900">
-        <TopNav />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/search" element={<SearchResultsPage />} />
-          <Route path="/product/:id" element={<ProductDetailPage />} />
-          <Route path="/product/:id/photos" element={<PhotoReviewsPage />} />
-          <Route path="/product/:id/reviews" element={<AllReviewsPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/mypage" element={<MyPage />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </div>
+      <AuthProvider>
+        <div className="min-h-screen bg-white selection:bg-mint-200 selection:text-mint-900">
+          <TopNav />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/search" element={<SearchResultsPage />} />
+            <Route path="/product/:id" element={<ProductDetailPage />} />
+            <Route path="/product/:id/reviews" element={<AllReviewsPage />} />
+            <Route path="/product/:id/photos" element={<PhotoReviewsPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/mypage" element={<MyPage />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </div>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
